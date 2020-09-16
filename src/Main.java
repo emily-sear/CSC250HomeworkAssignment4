e23fceb20e4d1b920b511a30a1949480062b7a25
emily-sear <61364884+emily-sear@users.noreply.github.com>
2020-09-16 15:52:37 -0500

    Initial Push

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main 
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter a number");
+		String inputedNum = input.nextLine();
+		
+		System.out.println("Your binary number in decimal is: " + Main.decimalToHex(Main.binaryToDecimal(inputedNum)));
+		
+		
+		
+	}
+	public static int binaryToDecimal(String binaryNumber)
+	{
+		int answer = 0;
+		int exponentNum = binaryNumber.length() -1;
+		
+		for(int i = 0; i < binaryNumber.length() ; i++)
+		{
+			int holder = Integer.parseInt(binaryNumber.valueOf(binaryNumber.charAt(i)));
+			//System.out.println(holder);
+			
+			holder = holder * (int)Math.pow(2,exponentNum);
+			//System.out.println(holder);
+			
+			answer = answer + holder;
+			//System.out.println(holder);
+			exponentNum--;
+		}
+		return answer;
+	}
+	public static String decimalToHex(int decimal)
+	{
+		String answer = ""; 
+		String map = "0123456789ABCDEF";
+		
+		while(decimal != 0)
+		{
+			answer = map.charAt(decimal%16) + answer;
+			decimal = decimal/16;
+		}
+		return answer;
+	}
+}
