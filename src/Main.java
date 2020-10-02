dccb3a71a3ec155da1738d7f72ead37846eaa952
emily-sear <61364884+emily-sear@users.noreply.github.com>
2020-10-01 19:38:37 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,8 @@ public class Main
 		System.out.println("Please enter a number");
 		String inputedNum = input.nextLine();
 		
-		System.out.println("Your binary number in decimal is: " + Main.decimalToHex(Main.binaryToDecimal(inputedNum)));
-		
+		System.out.println("Your binary number in hexidecimal is: " + Main.decimalToHex(Main.binaryToDecimal(inputedNum)));
+		System.out.println("Your binary number in hexidecimal is: " + Main.binaryToHex(inputedNum));
 	}
 	
 	public static int binaryToDecimal(String binaryNumber)
@@ -49,6 +49,47 @@ public class Main
 		}
 		
 		return sum;
+	}
+	
+	public static String padZeroForHex(String bin)
+	{
+		//only goes through the length of the string %4 amount of times... 
+		//This allows you to have groups of 4 bits... you are able to group your binary num into groups of 4
+		int zerosToAdd = 4 - (bin.length()%4);
+		//System.out.println(zerosToAdd);
+		for(int i = 0; i < zerosToAdd; i++)
+		{
+			bin = "0" + bin;
+		}
+		return bin;
+	}
+	
+	static String nibbleToHextet(String nibble)
+	{
+		String[] nibbles = {"0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000",
+				"1001", "1001", "1010", "1011", "1100", "1101", "1110", "1111"};
+		String map = "0123456789ABCDEF";
+		
+		for(int i = 0; i < nibbles.length; i++)
+		{
+			//equals() does this method contains the same characters as another String
+			if(nibbles[i].equals(nibble))
+			{
+				return "" + map.charAt(i);
+			}
+		}
+		return "-1";
+	}
+	
+	public static String binaryToHex(String binaryNumber)
+	{	
+		String answer = "";
+		String paddedBin = Main.padZeroForHex(binaryNumber);
+		for(int i = 0; i < paddedBin.length(); i+=4)
+		{
+			answer = answer + Main.nibbleToHextet("" + binaryNumber.charAt(i) + binaryNumber.charAt(i+1) + binaryNumber.charAt(i+2) + binaryNumber.charAt(i+3));
+		}
+		return answer;
 	}
 	
 	public static String decimalToHex(int decimal)
